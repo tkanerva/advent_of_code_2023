325664b78df4a62bf971c68bdfa5a09baf40d4de
Topi Kanerva <topi.kanerva@buutti.com>
2023-12-09 19:35:43 +0200

    forgot to add the solution for Day 7 part 2

---
+++ 7/puzzle2.ex
@@ -0,0 +1,173 @@
+defmodule Foo do
+
+  def sampletext() do
+    """
+    32T3K 765
+    T55J5 684
+    KK677 28
+    KTJJT 220
+    QQQJA 483
+    """
+  end
+
+  def readfile() do
+    {:ok, data} = File.read("input")
+    data
+  end
+
+  def cardvalues(), do: "J23456789TQKA"
+
+  def sym_val_map() do
+    # a dictionary comprehension
+    for {x, idx} <- String.graphemes(cardvalues) |> Enum.with_index, into: %{}, do: {x, idx+1}
+  end
+  
+  def parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split()
+    |> then(fn [a, b] -> {a, Integer.parse(b) |> elem(0)} end)
+  end
+
+  def string_to_hand(str) do
+    str
+    |> String.graphemes()
+    |> Enum.map(fn x -> Map.get(sym_val_map, x) end)
+  end
+
+  def hand_to_string(hand) do
+    invmap = for {k, v} <- sym_val_map, into: %{}, do: {v, k}
+    hand
+    |> Enum.map(fn revx -> Map.get(invmap, revx) end)
+    |> Enum.join()
+    |> IO.inspect
+  end
+  
+  def eval_hand(hand_str) do
+    # ordering puts the same cards adjacent to each other
+    ordered = hand_str |> string_to_hand |> Enum.sort
+
+    # for evaluation purposes, a joker acts as a wildcard
+    # try to count the number of Jokers in the beginning.
+    n_jokers = Enum.count(ordered, &(&1 == 1))
+    ordered = ordered |> Enum.filter(&(&1 != 1))
+    hand_type =
+      case ordered do
+        [] -> :fiveofakind
+        [a] -> :fiveofakind
+        [a, a] -> :fiveofakind
+        [a, a, a] -> :fiveofakind
+        [a, a, a, a] -> :fiveofakind
+
+        [a, a, a, a, a ] -> :fiveofakind
+
+        [a, a, a, a, _ ] -> :fourofakind
+        [_, a, a, a, a ] -> :fourofakind
+        [a, a, a, _] -> :fourofakind
+       	[_, a, a, a] -> :fourofakind
+        [a, a, _ ] -> :fourofakind
+        [_, a, a ] -> :fourofakind
+        [a, b] -> :fourofakind
+        
+        [a, a, b, b, b ] -> :fullhouse
+        [a, a, a, b, b ] -> :fullhouse
+
+        [_, b, b, b ] -> :fullhouse
+        [a, a, b, b ] -> :fullhouse
+        [_, b, b ] -> :fullhouse
+        [b, b, _ ] -> :fullhouse
+	  
+        [a, a, a, _, _ ] -> :threeofakind
+        [a, a, _, _ ] -> :threeofakind
+		
+        [_, a, a, a, _ ] -> :threeofakind
+        [_, a, a, _ ] -> :threeofakind
+		  
+        [_, _, a, a, a ] -> :threeofakind
+        [_, _, a, a ] -> :threeofakind
+       	[_, _, a ] -> :threeofakind
+		    
+        [a, a, _, c, c ] -> :twopair
+        [a, _, c, c ] -> :twopair
+        [a, a, _, c ] -> :twopair
+	      
+        [a, a, c, c, _ ] -> :twopair
+        [_, a, a, c, c ] -> :twopair
+          
+        [a, a, _, _, _ ] -> :onepair
+        [_, a, a, _, _ ] -> :onepair
+        [_, _, a, a, _ ] -> :onepair
+        [_, _, _, a, a ] -> :onepair
+
+        [a, b, c, d ] -> :onepair
+        
+        [a, b, c, d, e ] -> :highcard
+        
+        _ -> nil
+      end
+    { hand_str |> string_to_hand, hand_type }
+  end
+
+  def calculate_strength({cards, handtype} = hand) do
+    IO.inspect( [cards, handtype], charlists: :as_lists, label: :calculate_strength)
+    case handtype do
+      :fiveofakind -> 7
+      :fourofakind -> 6
+      :fullhouse -> 5
+      :threeofakind -> 4
+      :twopair -> 3
+      :onepair -> 2
+      :highcard -> 1
+    end
+  end
+
+  def calculate_for_sort({cards, handtype} = hand) do
+    val = 0x100_000 * calculate_strength(hand)
+    val = val + 0x10_000 * Enum.at(cards, 0)
+    val = val + 0x1_000 * Enum.at(cards, 1)
+    val = val + 0x100 * Enum.at(cards, 2)
+    val = val + 0x10 * Enum.at(cards, 3)
+    val = val + 0x1 * Enum.at(cards, 4)
+    
+    val
+  end
+  
+  def hand_sorter(a, b) do
+    IO.inspect a, label: :sorter, charlists: :as_lists
+    {ah, a_bid} = a
+    {bh, b_bid} = b
+    calculate_for_sort(ah) <= calculate_for_sort(bh)
+  end
+  
+  def rank_hands(hands_bids) do
+    # sort by the strength of the hand
+    n = length(hands_bids)
+    Enum.zip(1..n,  
+      hands_bids |> Enum.sort(&hand_sorter/2) 
+    ) |> IO.inspect( charlists: :as_lists, limit: :infinity)
+  end
+end
+
+#input = Foo.sampletext |> String.split("\n") |> Enum.reverse |> tl |> Enum.reverse 
+input = Foo.readfile |> String.split("\n") |> Enum.reverse |> tl |> Enum.reverse
+nrows = length(input)
+IO.inspect(nrows, label: "nrows")
+data = Enum.map(input, &Foo.parse_line/1)
+#IO.inspect data
+
+hands_bids =
+data
+|> Enum.map(fn {handstr, bid} -> { Foo.eval_hand(handstr), bid } end)
+|> IO.inspect(label: :hands, charlists: :as_lists, limit: :infinity)
+
+hands_bids
+|> Enum.map(fn {hand, bid} -> hand end)
+|> Enum.map(&Foo.calculate_strength/1 )
+|> IO.inspect(charlists: :as_lists)
+
+ranked = Foo.rank_hands(hands_bids) # |> IO.inspect(charlists: :as_lists)
+
+res = ranked |> Enum.map(fn {rank, {hand, bid}} -> rank * bid end)
+IO.puts ( inspect(Enum.sum(res)))
+
+#IO.inspect res, limit: :infinity, charlists: :as_lists
